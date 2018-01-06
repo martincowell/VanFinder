cfa2959f5fb3dab07660bc8a26ad73ee766dc08f
martincowell <martinimages@gmail.com>
2018-01-05 23:05:37 -0800

    Jan 05 data scrub

--- data-generation/van_visualizer.py
+++ data-generation/van_visualizer.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import json
 
-sets = {'20171231_transit': 'red', '20180105_sprinter': 'black', '20171231_sprinter': 'white'}
+sets = {'20171231_sprinter': 'black', '20180105_sprinter': 'red'}
 
 fig, ax = plt.subplots()
 for filename, color in sets.items():
     with open(filename + '.json') as data_file:
         data = json.load(data_file)
     df = pd.DataFrame(data)
-    ax.scatter(df.mileages, df.prices, s=12, color=color, alpha=1, label=filename)
+    ax.scatter(df.mileages, df.prices, s=12, color=color, alpha=0.3, label=filename)
 
 # with open('vans_20171231_sprinter.json') as data_file:
 #     data = json.load(data_file)
@@ -46,4 +46,5 @@ plt.xlim(0, 250000)
 plt.ylim(0, 60000)
 ax.legend()
 
+plt.savefig('20171231_sprinter.pdf')
 plt.show()
