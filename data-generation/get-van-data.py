2fb6ce6bd2ab30cb2b3ea39fdd02a03619e6e2bf
martincowell <martinimages@gmail.com>
2018-01-06 03:22:24 -0800

    trying to add cars.com. get-van-data.py does not work yet. stuck on lines 117-133: identifying price and mileage data

--- data-generation/get-van-data.py
+++ data-generation/get-van-data.py
@@ -22,7 +22,7 @@ page = 1
 
 driver = webdriver.Firefox()
 
-def get_car_data(car_id, zip, distance, page):
+def get_cargurus(car_id, zip, distance, page):
     url = 'https://www.cargurus.com/Cars/inventorylisting/viewDetailsFilterViewInventoryListing.action?sourceContext=' \
           'carGurusHomePage_false_0&' \
           'newSearchFromOverviewPage=true&' \
@@ -54,6 +54,7 @@ def get_car_data(car_id, zip, distance, page):
     reg_exp = re.compile('listing_[0-9]+')
     results = bs.find(id='listingsDiv').find_all('div', id=reg_exp)
     print('---got all listings')
+    print(results)
     # print(list(results))
 
     # get price data
@@ -86,17 +87,72 @@ def get_car_data(car_id, zip, distance, page):
     #return dict(mileages=mileages, prices=prices, IMVs=IMVs)
     return dict(mileages=mileages, prices=prices)
 
-def save_as_json(data):
-    file = open('csv/vans.json', 'w', encoding='utf8')
+
+def get_carscom(car_id, zip, distance, page):
+    url = 'https://www.cars.com/for-sale/searchresults.action/?mdId=22183&' \
+        'mkId=20028&' \
+        'page=1&' \
+        'perPage=100&' \
+        'rd=99999&' \
+        'searchSource=PAGINATION&' \
+        'sort=relevance&' \
+        'stkTypId=28881&' \
+        'trId=22711&' \
+        'zc=90278'
+
+    print(url)
+
+    driver.get(url)
+    # if page == 1:
+    time.sleep(randint(5, 8))
+    html = driver.page_source
+    bs = BeautifulSoup(html, 'html.parser')
+
+    reg_exp = re.compile('listing-[0-9]+')
+    results = bs.find(id='listings').find_all('div', id=reg_exp)
+    print('---got all listings')
+    #   print(results)
+
+    # get price data
+    for res in results:
+        price_tag = res.find(string='listing-row__price')
+        print(price_tag)
+        price = 0
+        if price_tag is not None:
+            price = int(price_tag.find_parent('p').find('span').find_all('span')[0].get_text().strip().replace('$', '').replace(',', '').replace('No Price Listed','0'))
+        prices.append(price)
+    print(prices)
+
+    # get mileage data
+    for res in results:
+        mile_tag = res.find(string='listing-row__mileage')
+        mileage = 0
+        if mile_tag is not None:
+            mileage = int(mile_tag.find_parent('p').get_text().strip().replace('Mileage: ', '').replace(' mi', '').replace(',', '').replace('N/A', '0'))
+        mileages.append(mileage)
+    print(mileages)
+
+    return dict(mileages=mileages, prices=prices)
+
+def save_as_json(fname, data):
+    file = open('csv/' + fname +'.json', 'w', encoding='utf8')
     json_data = json.dumps(data, ensure_ascii=False)
     file.write(json_data)
     file.close()
 
 prices = []
 mileages = []
-# IMVs = []
-for i in range(1, 10 + 1):
-    data = get_car_data(car_id, zip, distance, i)
-    save_as_json(data)
-    print('page ' + str(i))
 
+if 0:
+    fname = 'cargurus'
+    for i in range(1, 10 + 1):
+        data = get_cargurus(car_id, zip, distance, i)
+        save_as_json(fname, data)
+        print('page ' + str(i))
+
+if 1:
+    fname = 'carscom'
+    for i in range(1, 10 + 1):
+        data = get_carscom(car_id, zip, distance, i)
+        save_as_json(fname, data)
+        print('page ' + str(i))
